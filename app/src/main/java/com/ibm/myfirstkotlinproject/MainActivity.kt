dba422f7a14c901cfba365922f1936aa70dbea55
ShruthiS9 <shruthi.sreeramoju@ibm.com>
2021-07-28 16:18:55 +0530

    all grid files are committed

--- app/src/main/java/com/ibm/myfirstkotlinproject/MainActivity.kt
+++ app/src/main/java/com/ibm/myfirstkotlinproject/MainActivity.kt
@@ -2,10 +2,12 @@ package com.ibm.myfirstkotlinproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("Hello!!!", "MainActivity")
     }
 }
